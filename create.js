6ccb57e0f59c9a36fb26696795b2b1cad2ad59ac
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-07-20 21:35:33 +0900

    綺麗になった

--- create.js
+++ create.js
@@ -1,28 +1,28 @@
 //変数の設定
-var progress =  document.getElementById('progress');
+var progress = document.getElementById('progress');
 var homework = document.getElementById('name');
 var homeworkinfo = [];
 var number;
 //テンプレートの設定
-function template(name, progress, number){
+function template(name, progress, number) {
     return `{ "number": ${encodeURIComponent(number)}, "name": "${encodeURIComponent(name)}", "progress": ${encodeURIComponent(progress)} }`;
 }
 //カウントの設定
-function countup(name, progress, number){
+function countup(name, progress, number) {
     number = JSON.parse(template(name, progress, number)).number + 1;
     window.localStorage.setItem('number', number);
 }
 //宿題の追加
-function add_homework(name, progress, number){
+function add_homework(name, progress, number) {
     window.localStorage.setItem(number, template(name, progress, number));
     countup(name, progress, number);
 }
 //プログレスの取得と表示
-progress.addEventListener('change', function(){
+progress.addEventListener('change', function () {
     var textelement = document.getElementById('progresst');
     textelement.textContent = progress.value + '%';
 });
-document.getElementById('button').addEventListener('click' , function(){
-     window.localStorage.getItem('number')
-    add_homework(homework.value,progress.value,window.localStorage.getItem('number'));
+document.getElementById('button').addEventListener('click', function () {
+    window.localStorage.getItem('number')
+    add_homework(homework.value, progress.value, window.localStorage.getItem('number'));
 });
