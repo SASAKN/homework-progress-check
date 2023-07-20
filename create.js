2db607715a4e1a07d0de481425ab3388b3010422
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-07-20 19:40:33 +0900

    Update

--- create.js
+++ create.js
@@ -3,3 +3,9 @@ function add_homework(name, progress, number){
     var temp = `${encodeURIComponent(number)},${encodeURIComponent(name)},${encodeURIComponent(progress)}`;
     document.cookie = temp;
 }
+//プログレスの取得と表示
+var progress =  document.getElementById('progress');
+progress.addEventListener('change', function(){
+    var textelement = document.getElementById('progresst');
+    textelement.textContent = progress.value + '%';
+});
