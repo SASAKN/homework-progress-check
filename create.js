167e8ce2d3afe39f975c2161b3b188dc4bb3fa10
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-07-20 20:53:37 +0900

    Update

--- create.js
+++ create.js
@@ -1,11 +1,16 @@
+//変数の設定
+var progress =  document.getElementById('progress');
+var homework = document.getElementById('name');
 //関数の設定
 function add_homework(name, progress, number){
-    var temp = `${encodeURIComponent(number)},${encodeURIComponent(name)},${encodeURIComponent(progress)}`;
-    document.cookie = temp;
+    var temp = `{ "number": ${encodeURIComponent(number)}, "name": "${encodeURIComponent(name)}", "progress": ${encodeURIComponent(progress)} }`;
+    document.cookie += temp;
 }
 //プログレスの取得と表示
-var progress =  document.getElementById('progress');
 progress.addEventListener('change', function(){
     var textelement = document.getElementById('progresst');
     textelement.textContent = progress.value + '%';
+});
+document.getElementById('button').addEventListener('click' , function(){
+    add_homework(homework.value,progress.value,0);
 });
