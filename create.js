71b1fa6f32ee9c9b72dbe3c783ad3b12415fedef
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-07-20 21:35:17 +0900

    動作する

--- create.js
+++ create.js
@@ -2,15 +2,20 @@
 var progress =  document.getElementById('progress');
 var homework = document.getElementById('name');
 var homeworkinfo = [];
-//関数の設定
+var number;
+//テンプレートの設定
 function template(name, progress, number){
     return `{ "number": ${encodeURIComponent(number)}, "name": "${encodeURIComponent(name)}", "progress": ${encodeURIComponent(progress)} }`;
 }
+//カウントの設定
+function countup(name, progress, number){
+    number = JSON.parse(template(name, progress, number)).number + 1;
+    window.localStorage.setItem('number', number);
+}
+//宿題の追加
 function add_homework(name, progress, number){
-    let temporary = JSON.parse(template(name, progress, number));
-    let tnumber = temporary.number + 1;
-    window.localStorage.setItem('number', tnumber);
     window.localStorage.setItem(number, template(name, progress, number));
+    countup(name, progress, number);
 }
 //プログレスの取得と表示
 progress.addEventListener('change', function(){
