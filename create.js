f0ecf22cd0aeda20c7af79a32c69da3cd54b049f
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-07-21 17:38:23 +0900

    おとづれてないとき

--- create.js
+++ create.js
@@ -2,22 +2,31 @@
 var progress = document.getElementById('progress');
 var homework = document.getElementById('name');
 var homeworkinfo = [];
-var number = 1;
+//初回に訪れた時
+if(document.cookie === 'visit'){
+    console.log('このユーザーは過去におとづれたことがあるのでlocal Storageの操作を行いません。');
+}else if(document.cookie === 'delete'){
+    localStorage.setItem('number', 0);
+    document.cookie = 'visit';
+}else{
+    localStorage.setItem('number', 0);
+    document.cookie = 'visit';
+}
 //テンプレートの設定
 function template(name, progress, number) {
     return `{ "number": ${encodeURIComponent(number)}, "name": "${encodeURIComponent(name)}", "progress": ${encodeURIComponent(progress)} }`;
 }
 //カウントの設定
-function countup(name, progress, number) {
-    number = JSON.parse(template(name, progress, number)).number;
-    number++;
-    window.localStorage.setItem('number', number);
+function countup(number) {
+    var a  = localStorage.getItem('number');
+    a++;
+    window.localStorage.setItem('number', a);
 }
 //宿題の追加
 function add_homework(name, progress, number) {
     window.localStorage.setItem(number, template(name, progress, number));
     console.log(template(name, progress, number));
-    countup(name, progress, number);
+    countup(number);
 }
 //プログレスの取得と表示
 progress.addEventListener('change', function () {
@@ -25,7 +34,6 @@ progress.addEventListener('change', function () {
     textelement.textContent = progress.value + '%';
 });
 document.getElementById('button').addEventListener('click', function () {
-    window.localStorage.getItem('number');
     document.getElementById('button').textContent = '追加しました'
     document.getElementById('button').style.backgroundColor = '#00ffc8';
     setTimeout(function(){
@@ -38,6 +46,7 @@ document.getElementById('delete').addEventListener('click', function () {
     var prompt = window.confirm('本当に削除しますか？');
     if (prompt) {
         window.localStorage.clear();
+        document.cookie = 'delete';
         location.reload();
     } else {
         location.reload();
